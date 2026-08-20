a9f456ca5e4b0ed6ba4bab08e3fb21b5226a4319
WATERMELON <WATERMELON@DESKTOP-4UPNDSR>
2026-08-21 01:10:14 +0800

    card practice

---
+++ react-project/src/Card.jsx
@@ -0,0 +1,14 @@
+function Card() {
+    return(
+        <div className="card">
+            <img src="src/assets/hero.png" alt="" />
+            <div className="detail">
+                <h2>vite</h2>
+                <p>welcome to vite!</p>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Card
