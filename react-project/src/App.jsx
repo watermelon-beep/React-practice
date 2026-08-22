63e717802320d560b967c0f9c902b59632073ec5
buffy duck <kianabante58@gmail.com>
2026-08-22 15:19:28 +0800

    refactor: update Card component details and improve App logic

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,16 +1,22 @@
 import Card from './Card.jsx'
 import hero from './assets/hero.png'
-import  charImg from './assets/charImg.png'
+import charImg from './assets/charImg.png'
+import Proptypes from 'prop-types'
 
 function App() {
 
+  let isShow = true;
+
   return (
     <>
-      <Card img={hero} name="kian" detail="pogi"/>
-      <Card img={charImg} name="flexi" detail="stupid bitch"/>
-      <Card />
+      {isShow ? (<Card img={hero} name="kian" detail="pogi"/>) :
+      (<Card img={charImg} name="scept" detail="shitty pants" />)}
     </>
   );
 }
+
+App.proptypes = {
+  isShow: Proptypes.bool
+};
 
 export default App
--- react-project/src/Card.jsx
+++ react-project/src/Card.jsx
@@ -4,13 +4,14 @@ import ReactImg from './assets/react.svg'
 function Card({
     img = ReactImg,
     name = "elena",
-    detail = "shitty pants",
+    detail = "dummy dumb dumb",
 }) {
+     
     return(
         <div className="row-part">
             <div className="card">
                 <img src= {img} alt="" />
-                <div className="detail">
+                   <div className="detail">
                     <h2>{name}</h2>
                     <p>{detail}</p>
                 </div>
@@ -22,7 +23,7 @@ function Card({
 Card.propTypes = {
     img: PropTypes.string,
     name: PropTypes.string,
-    detail: PropTypes.string,
+    detail: PropTypes.string   
 };
 
 
